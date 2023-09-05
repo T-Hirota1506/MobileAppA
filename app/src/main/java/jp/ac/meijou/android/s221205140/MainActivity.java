5977772303f750e865383dc023646e77cf3483b5
廣田拓也 <221205140@ccmailg.meijo-u.ac.jp>
2023-09-05 13:41:16 +0900

    ボタンを押したらテキスト内容が変わるように変更した

--- app/src/main/java/jp/ac/meijou/android/s221205140/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s221205140/MainActivity.java
@@ -1,14 +1,48 @@
 package jp.ac.meijou.android.s221205140;
 
+import static jp.ac.meijou.android.s221205140.R.drawable.baseline_elderly_24;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.TextView;
+
+import jp.ac.meijou.android.s221205140.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding =ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener((View view) -> {
+            var text=binding.editTextText.getText().toString();
+        });
+
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                binding.text.setText(editable.toString());
+            }
+        });
     }
+
+
 }
